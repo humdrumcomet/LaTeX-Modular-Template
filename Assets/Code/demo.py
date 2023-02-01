31d597cc0faa4851a4086c5f155bd43ed057ee80
Aaron English <humdrumcomet@protonmail.com>
2023-02-01 17:12:21 -0500

    Emptying file out for use as actual template

--- Assets/Code/demo.py
+++
@@ -1,9 +0,0 @@
-def string2bits(s=''):
-    return [bin(ord(x))[2:].zfill(8) for x in s]
-
-def bits2string(b=None):
-    return ''.join([chr(int(x, 2)) for x in b])
-
-s = 'Hello, World!'
-b = string2bits(s)
-s2 = bits2string(b)
